11757f2fc576fbf625834f71646f9fbbadf7d4e6
Borja Outerelo <borjaouterelo@gmail.com>
2017-12-01 08:03:53 +0100

    Refs #2375. Adds licenses

--- src/c/xrce_protocol_serialization.c
+++ src/c/xrce_protocol_serialization.c
@@ -1,3 +1,17 @@
+// Copyright 2017 Proyectos y Sistemas de Mantenimiento SL (eProsima).
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #include "xrce_protocol_serialization.h"
 
 void serialize_ClientKey(MicroBuffer* buffer, const ClientKey* input)
@@ -1005,5 +1019,3 @@ void deserialize_PackedSamplesPayloadData(MicroBuffer* buffer, PackedSamplesPayl
     deserialize_BaseObjectReply(buffer, &output->reply, aux);
     deserialize_PackedSamples(buffer, &output->packed_samples, aux);
 }
-
-
