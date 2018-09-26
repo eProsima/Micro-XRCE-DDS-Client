a4e5e566a830f97b17cd22f0834152799f87fbc6
Luis Enrique Muñoz Martín <lemunozm@gmail.com>
2018-09-26 16:18:23 +0200

    Refs #3233. Modified and completed output best effort tests.

--- test/unitary/streams/output_best_effort/OutputBestEffortStream.cpp
+++ test/unitary/streams/output_best_effort/OutputBestEffortStream.cpp
@@ -1,6 +1,5 @@
 #include <gtest/gtest.h>
 
-#define SUBMESSAGE_MAX 256
 #define STREAM_BUFFER_SIZE 512
 #define OFFSET 4
 
@@ -10,6 +9,66 @@ public:
     OutputBestEffortStreamTest()
     {
 //        init_output_best_effort_stream(&stream, buffer, STREAM_BUFFER_SIZE, OFFSET);
+    }
+
+    void write_submessage(bool expected)
+    {
+        size_t previous_writer = stream.writer;
+
+        mcBuffer mb;
+        bool available_to_write = prepare_best_effort_buffer_to_write(&stream, submessage.size(), &mb);
+        bool serialized = mc_serialize_array_char(&mb, submessage.c_str(), static_cast<uint16_t>(submessage.size()));
+
+        if(expected)
+        {
+            ASSERT_TRUE(available_to_write);
+            ASSERT_TRUE(serialized);
+            ASSERT_EQ(previous_writer + submessage.size(), stream.writer);
+            for(size_t i = 0; i < submessage.size(); i++)
+            {
+                ASSERT_EQ(submessage[i], buffer[previous_writer + i]);
+            }
+        }
+        else
+        {
+            ASSERT_FALSE(available_to_write);
+            ASSERT_FALSE(serialized);
+            ASSERT_EQ(previous_writer, stream.writer);
+        }
+    }
+
+    void send_message()
+    {
+        mrSeqNum previous_seq_num = stream.last_send;
+        size_t previous_writer = stream.writer;
+
+        uint8_t* output_buffer; size_t length; mrSeqNum seq_num;
+        bool available_to_send = prepare_best_effort_buffer_to_send(&stream, &output_buffer, &length, &seq_num);
+
+        ASSERT_EQ(previous_seq_num, seq_num_sub(seq_num, 1));
+        ASSERT_EQ(previous_writer, length);
+        ASSERT_EQ(stream.writer, stream.offset);
+        ASSERT_TRUE(available_to_send);
+    }
+
+    void write_submessages_all(bool inside)
+    {
+        while(stream.writer + submessage.size() < stream.size)
+        {
+            write_submessage(true);
+        }
+
+        if(!inside)
+        {
+            write_submessage(false);
+        }
+    }
+
+    void reset()
+    {
+        reset_output_best_effort_stream(&stream);
+        ASSERT_EQ(stream.writer, OFFSET);
+        ASSERT_EQ(stream.last_send, UINT16_MAX);
     }
 
     virtual ~OutputBestEffortStreamTest()
@@ -22,31 +81,42 @@ protected:
 //    uxrOutputBestEffortStream stream;
 };
 
-TEST_F(OutputBestEffortStreamTest, WriteSubmessage)
+TEST_F(OutputBestEffortStreamTest, WriteOneSubmessage)
 {
-    //TODO
+    write_submessage(true);
 }
 
-/*TEST_F(OutputBestEffortStreamTest, WriteSubmessage)
+TEST_F(OutputBestEffortStreamTest, WriteSubmessageFull)
 {
-    // Write
-    ucdrBuffer mb;
-    bool available_to_write = prepare_best_effort_buffer_to_write(&stream, submessage.size(), &mb);
-    ucdr_serialize_array_char(&mb, submessage.c_str(), static_cast<uint16_t>(submessage.size()));
+    write_submessages_all(true);
+}
 
-    // Send
-    uxrSeqNum init_seq_num = stream.last_send;
-    uint8_t* output_buffer; size_t length; uxrSeqNum seq_num;
-    bool available_to_send = prepare_best_effort_buffer_to_send(&stream, &output_buffer, &length, &seq_num);
+TEST_F(OutputBestEffortStreamTest, WriteSubmessageError)
+{
+    write_submessages_all(false);
+}
 
-    // Check
-    ASSERT_EQ(available_to_write, true);
-    ASSERT_EQ(available_to_send, true);
-    ASSERT_EQ(submessage.size(), length - OFFSET);
-    ASSERT_EQ(init_seq_num, seq_num_sub(seq_num, 1));
-    for(size_t i = 0; i < submessage.size(); i++)
-    {
-        ASSERT_EQ(submessage[i], output_buffer[OFFSET + i]);
-    }
-}*/
+TEST_F(OutputBestEffortStreamTest, SendOneSubmessage)
+{
+    write_submessage(true);
+    send_message();
+}
 
+TEST_F(OutputBestEffortStreamTest, SendSubmessageFull)
+{
+    write_submessages_all(true);
+    send_message();
+}
+
+TEST_F(OutputBestEffortStreamTest, SendSubmessageError)
+{
+    write_submessages_all(false);
+    send_message();
+}
+
+TEST_F(OutputBestEffortStreamTest, Reset)
+{
+    write_submessages_all(true);
+    send_message();
+    reset();
+}
