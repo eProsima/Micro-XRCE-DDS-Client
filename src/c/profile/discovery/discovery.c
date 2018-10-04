e218fdf6e80fe200627766038e7b2c85e0f40cb0
Luis Enrique Muñoz Martín <lemunozm@gmail.com>
2018-10-04 08:27:14 +0200

    Refs #3446. Make basic discovery implementation.

--- src/c/profile/discovery/discovery.c
+++ src/c/profile/discovery/discovery.c
@@ -6,12 +6,24 @@
 #include <micrortps/client/core/log/log.h>
 #include <micrortps/client/transport.h>
 
-#include <microcdr/microcdr.h>
+#include <string.h>
 
 #define GET_INFO_MSG_SIZE   8
 #define GET_INFO_REQUEST_ID 9
 
-static void write_get_info(mcBuffer* mb);
+typedef struct CallbackData
+{
+    AgentAddress* choosen;
+    mrOnAgentFound on_agent;
+    void* args;
+
+} CallbackData;
+
+
+static void write_get_info_message(mcBuffer* mb);
+static bool listen_read_info_message(mrCommunication* communication, int period, CallbackData* callback);
+static bool read_info_message(mcBuffer* mb, CallbackData* callback);
+static bool process_info(CallbackData* callback, Time_t timestamp, TransportLocator* transport);
 
 //==================================================================
 //                             PUBLIC
@@ -27,41 +39,102 @@ bool mr_discovery_agents_multicast(int time, int period, mrOnAgentFound on_agent
 bool mr_discovery_agents_unicast(int time, int period, mrOnAgentFound on_agent_func, void* args, AgentAddress* choosen,
                                  const AgentAddress* agent_list, size_t agent_list_size)
 {
-    (void) time; (void) period; (void) on_agent_func; (void) args; (void) choosen;
+    (void) time;
+
+    CallbackData callback = {choosen, on_agent_func, args};
 
     uint8_t output_buffer[MR_CONFIG_UDP_TRANSPORT_MTU];
     mcBuffer mb;
     mc_init_buffer(&mb, output_buffer, MR_CONFIG_UDP_TRANSPORT_MTU);
-    write_get_info(&mb);
+    write_get_info_message(&mb);
     size_t message_length = mc_buffer_length(&mb);
 
-    for(size_t i = 0; i < agent_list_size; ++i)
+    bool consumed = false;
+    for(size_t i = 0; i < agent_list_size && !consumed; ++i)
     {
         mrUDPTransport transport;
         if(mr_init_udp_transport(&transport, agent_list[i].ip, agent_list[i].port))
         {
-            (void) transport.comm.send_msg(&transport, output_buffer, message_length);
+            bool sent = transport.comm.send_msg(&transport, output_buffer, message_length);
             DEBUG_PRINT_MESSAGE(SEND, output_buffer, message_length, 0);
+
+            if(sent)
+            {
+                consumed = listen_read_info_message(&transport.comm, period, &callback);
+            }
         }
     }
 
-    //TODO
-    return false;
+    return consumed;
 }
 
 //==================================================================
 //                             INTERNAL
 //==================================================================
 
-void write_get_info(mcBuffer* mb)
+void write_get_info_message(mcBuffer* mb)
 {
     GET_INFO_Payload payload;
     payload.base.request_id = (RequestId){{0x00, GET_INFO_REQUEST_ID}};
     payload.base.object_id = OBJECTID_AGENT;
-    payload.info_mask = 0;
+    payload.info_mask = INFO_CONFIGURATION | INFO_ACTIVITY;
 
     (void) mc_serialize_message_header(mb, 0x80, 0, 0, 0);
     (void) write_submessage_header(mb, SUBMESSAGE_ID_GET_INFO, GET_INFO_MSG_SIZE, 0);
     (void) serialize_GET_INFO_Payload(mb, &payload);
+}
+
+bool listen_read_info_message(mrCommunication* communication, int period, CallbackData* callback)
+{
+    bool consumed = false;
+    uint8_t* input_buffer; size_t length;
+
+    bool received = communication->recv_msg(communication->instance, &input_buffer, &length, period);
+    if(received)
+    {
+        DEBUG_PRINT_MESSAGE(RECV, input_buffer, length, 0);
+
+        mcBuffer mb;
+        mc_init_buffer(&mb, input_buffer, (uint32_t)length);
+        consumed = read_info_message(&mb, callback);
+    }
+
+    return consumed;
+}
+
+bool read_info_message(mcBuffer* mb, CallbackData* callback)
+{
+    bool consumed = false;
+    INFO_Payload payload;
+
+    if(deserialize_INFO_Payload(mb, &payload))
+    {
+        XrceVersion* version = &payload.object_info.config._.agent.xrce_version;
+        TransportLocator* transport = &payload.object_info.activity._.agent.address_seq.data[0];
+        Time_t timestamp = payload.object_info.config._.agent.agent_timestamp;
+
+        if(0 == memcmp(version->data, XRCE_VERSION.data, sizeof(XRCE_VERSION.data)))
+        {
+            consumed = process_info(callback, timestamp, transport);
+        }
+    }
+
+    return consumed;
+}
+
+bool process_info(CallbackData* callback, Time_t timestamp, TransportLocator* transport)
+{
+    bool consumed = false;
+    int64_t nanoseconds = (int64_t)timestamp.seconds + (int64_t)timestamp.nanoseconds * 1000000000L;
+
+    if(transport->format == ADDRESS_FORMAT_MEDIUM)
+    {
+        memcpy(callback->choosen->ip, transport->_.medium_locator.address, 4); //CHECK
+        callback->choosen->port = transport->_.medium_locator.locator_port;
+
+        consumed = callback->on_agent(callback->choosen, nanoseconds, callback->args);
+    }
+
+    return consumed;
 }
 
