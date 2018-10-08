7c1aefdef070f8354c1cd51f5bf79072c82d495c
Luis Enrique Muñoz Martín <lemunozm@gmail.com>
2018-10-08 09:05:22 +0200

    Adapted log to fix windows issue.

--- src/c/core/log/log_internal.h
+++ src/c/core/log/log_internal.h
@@ -41,11 +41,15 @@ extern "C"
 void uxr_print_message(int direction, uint8_t* buffer, size_t size, const uint8_t* client_key);
 void uxr_print_serialization(int direction, const uint8_t* buffer, size_t size);
 
+#if defined(UXR_MESSAGE_LOGS) || defined(UXR_SERIALIZATION_LOGS)
 #define DEBUG_PRINT_MESSAGE(direction, buffer, size, client_key) \
     do { \
         if (UXR_MESSAGE_LOGS_AVAILABLE) uxr_print_message(direction, buffer, size, client_key); \
         if (UXR_SERIALIZATION_LOGS_AVAILABLE) uxr_print_serialization(direction, buffer, size); \
-    } while (0);
+    } while (0)
+#else
+#define DEBUG_PRINT_MESSAGE(direction, buffer, size, client_key) do {} while(0)
+#endif
 
 #ifdef __cplusplus
 }
