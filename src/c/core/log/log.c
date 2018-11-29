e2fc2d3ad92be459da044a0bdecf4e2a976b39db
Luis Enrique Muñoz Martín <lemunozm@gmail.com>
2018-11-29 07:37:43 +0100

    Refs #3804. Updated log.

--- src/c/core/log/log.c
+++ src/c/core/log/log.c
@@ -82,6 +82,7 @@ static void print_write_data_data_submessage(const char* pre, const WRITE_DATA_P
 static void print_data_data_submessage(const char* pre, const DATA_Payload_Data* payload);
 static void print_acknack_submessage(const char* pre, const ACKNACK_Payload* payload);
 static void print_heartbeat_submessage(const char* pre, const HEARTBEAT_Payload* payload);
+static void print_fragment_submessage(const char* pre, uint16_t size, uint8_t flags);
 static void print_header(size_t size, int direction, uint8_t stream_id, uint16_t seq_num, const uint8_t* client_key);
 static void print_tail(int64_t initial_log_time);
 
@@ -219,7 +220,8 @@ void uxr_print_message(int direction, uint8_t* buffer, size_t size, const uint8_
 
             case SUBMESSAGE_ID_FRAGMENT:
             {
-                printf("%s[FRAGMENT SUBMESSAGE (not supported)]%s", RED, RESTORE_COLOR);
+                print_fragment_submessage(color, length, flags);
+
             } break;
 
             default:
@@ -530,6 +532,15 @@ void print_heartbeat_submessage(const char* pre, const HEARTBEAT_Payload* payloa
             pre,
             payload->first_unacked_seq_nr,
             payload->last_unacked_seq_nr,
+            RESTORE_COLOR);
+}
+
+void print_fragment_submessage(const char* pre, uint16_t size, uint8_t flags)
+{
+    printf("%s[FRAGMENT | size: %hu | %s]%s",
+            pre,
+            size,
+            (FLAG_LAST_FRAGMENT & flags) ? "last" : "first",
             RESTORE_COLOR);
 }
 
