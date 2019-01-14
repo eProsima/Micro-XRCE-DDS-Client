15a01165c2b62df9c896193378e2cd89906555e2
Luis Enrique Muñoz Martín <lemunozm@gmail.com>
2019-01-14 09:17:25 +0100

    Refs #4326. Added Input best effort unitary tests.

---
+++ test/unitary/session/streams/InputBestEffortStream.cpp
@@ -0,0 +1,53 @@
+
+#include <gtest/gtest.h>
+
+extern "C"
+{
+#include "../src/c/core/session/stream/seq_num.c"
+#include "../src/c/core/session/stream/input_best_effort_stream.c"
+}
+
+TEST(InputBestEffortStreamTest, Init)
+{
+    uxrInputBestEffortStream stream;
+    uxr_init_input_best_effort_stream(&stream);
+    EXPECT_EQ(UINT16_MAX, stream.last_handled);
+}
+
+TEST(InputBestEffortStreamTest, Reset)
+{
+    uxrInputBestEffortStream stream;
+    uxr_init_input_best_effort_stream(&stream);
+    uxr_reset_input_best_effort_stream(&stream);
+    EXPECT_EQ(UINT16_MAX, stream.last_handled);
+}
+
+TEST(InputBestEffortStreamTest, ReceiveExpected)
+{
+    uxrInputBestEffortStream stream;
+    uxr_init_input_best_effort_stream(&stream);
+    uxrSeqNum expected_seq_num = uxr_seq_num_add(stream.last_handled, 1);
+    bool available_to_read = uxr_receive_best_effort_message(&stream, expected_seq_num);
+    EXPECT_TRUE(available_to_read);
+    EXPECT_EQ(expected_seq_num, stream.last_handled);
+}
+
+TEST(InputBestEffortStreamTest, ReceiveNoExpectedLost)
+{
+    uxrInputBestEffortStream stream;
+    uxr_init_input_best_effort_stream(&stream);
+    uxrSeqNum expected_seq_num = uxr_seq_num_add(stream.last_handled, 2);
+    bool available_to_read = uxr_receive_best_effort_message(&stream, expected_seq_num);
+    EXPECT_TRUE(available_to_read);
+    EXPECT_EQ(expected_seq_num, stream.last_handled);
+}
+
+TEST(InputBestEffortStreamTest, ReceiveNoExpected)
+{
+    uxrInputBestEffortStream stream;
+    uxr_init_input_best_effort_stream(&stream);
+    uxrSeqNum expected_seq_num = stream.last_handled;
+    bool available_to_read = uxr_receive_best_effort_message(&stream, expected_seq_num);
+    EXPECT_FALSE(available_to_read);
+    EXPECT_EQ(expected_seq_num, stream.last_handled);
+}
