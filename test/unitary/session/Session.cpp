ab6bf6f516ecc7f4bc645c54117e707439f48738
Luis Enrique Muñoz Martín <lemunozm@gmail.com>
2019-01-23 16:01:52 +0100

    Added create and delete mock messages.

--- test/unitary/session/Session.cpp
+++ test/unitary/session/Session.cpp
@@ -1,211 +1,235 @@
-
-#include <gtest/gtest.h>
-
 extern "C"
 {
+#include "../src/c/core/serialization/xrce_protocol.c"
+#include "../src/c/core/serialization/xrce_header.c"
+#include "../src/c/core/serialization/xrce_subheader.c"
+
 #include "../src/c/core/session/stream/seq_num.c"
 #include "../src/c/core/session/stream/stream_id.c"
+#include "../src/c/core/session/stream/stream_storage.c"
+#include "../src/c/core/session/stream/input_best_effort_stream.c"
+#include "../src/c/core/session/stream/output_best_effort_stream.c"
+#include "../src/c/core/session/stream/input_reliable_stream.c"
+#include "../src/c/core/session/stream/output_reliable_stream.c"
+
+#include "../src/c/core/session/object_id.c"
+#include "../src/c/core/session/submessage.c"
+#include "../src/c/core/session/session_info.c"
+
+#undef UXR_MESSAGE_LOG
+#undef UXR_SERIALIZATION_LOG
 #include "../src/c/core/session/session.c"
 }
 
-#define BUFFER_SIZE           128
+#include <gtest/gtest.h>
+#include <string>
+
+#define MTU                   64
 #define HISTORY               4
 #define OFFSET                8
+
 
 class SessionTest : public testing::Test
 {
 public:
+    static SessionTest* current;
     SessionTest()
     {
-    }
+        //Necessary for the mocks
+        current = this;
 
-    virtual ~SessionTest()
-    {
-    }
+        comm.instance = NULL;
+        comm.mtu = 32;
+        comm.send_msg = send_msg;
+        comm.recv_msg = recv_msg;
+        comm.comm_error = comm_error;
 
-protected:
-    uxrSession session;
-    uint8_t ob_buffer[BUFFER_SIZE / HISTORY];
-    uint8_t or_buffer[BUFFER_SIZE];
-    uint8_t ir_buffer[BUFFER_SIZE];
+        uxr_init_session(&session, &comm, 0xAAAABBBB);
+
+        EXPECT_EQ(&comm, session.comm);
+        EXPECT_EQ(NULL, session.request_list);
+        EXPECT_EQ(NULL, session.status_list);
+        EXPECT_EQ(NULL, session.request_status_list_size);
+
+        EXPECT_EQ(NULL, session.on_status);
+        EXPECT_EQ(NULL, session.on_status_args);
+        EXPECT_EQ(NULL, session.on_topic);
+        EXPECT_EQ(NULL, session.on_topic_args);
+
+
+        uxrStreamId id = uxr_create_input_best_effort_stream(&session);
+        EXPECT_EQ(0, id.index);
+        EXPECT_EQ(BEST_EFFORT_STREAM_THRESHOLD, id.raw);
+        EXPECT_EQ(UXR_BEST_EFFORT_STREAM, id.type);
+        EXPECT_EQ(UXR_INPUT_STREAM, id.direction);
+
+        id = uxr_create_output_best_effort_stream(&session, output_best_effort_buffer, MTU);
+        EXPECT_EQ(0, id.index);
+        EXPECT_EQ(BEST_EFFORT_STREAM_THRESHOLD, id.raw);
+        EXPECT_EQ(UXR_BEST_EFFORT_STREAM, id.type);
+        EXPECT_EQ(UXR_OUTPUT_STREAM, id.direction);
+
+        id = uxr_create_input_reliable_stream(&session, input_reliable_buffer, MTU * HISTORY, HISTORY);
+        EXPECT_EQ(0, id.index);
+        EXPECT_EQ(RELIABLE_STREAM_THRESHOLD, id.raw);
+        EXPECT_EQ(UXR_RELIABLE_STREAM, id.type);
+        EXPECT_EQ(UXR_INPUT_STREAM, id.direction);
+
+        id = uxr_create_output_reliable_stream(&session, output_reliable_buffer, MTU * HISTORY, HISTORY);
+        EXPECT_EQ(0, id.index);
+        EXPECT_EQ(RELIABLE_STREAM_THRESHOLD, id.raw);
+        EXPECT_EQ(UXR_RELIABLE_STREAM, id.type);
+        EXPECT_EQ(UXR_OUTPUT_STREAM, id.direction);
+    }
 
 public:
+    uxrCommunication comm;
+    uxrSession session;
+    uint8_t output_best_effort_buffer[MTU];
+    uint8_t output_reliable_buffer[MTU * HISTORY];
+    uint8_t input_reliable_buffer[MTU * HISTORY];
+
+    static bool send_msg(void* instance, const uint8_t* buf, size_t len)
+    {
+        (void) instance; (void) buf; (void) len;
+        return true;
+    }
+
+    static bool recv_msg(void* instance, uint8_t** buf, size_t* len, int timeout)
+    {
+        (void) instance; (void) timeout;
+        static std::array<uint8_t, MTU> input_buffer;
+
+        if(std::string("CreateOk") == ::testing::UnitTest::GetInstance()->current_test_info()->name())
+        {
+            std::vector<uint8_t> message = {0x81, 0x00, 0x00, 0x00, 0x04, 0x01, 0x19, 0x00,
+                                            0x00, 0x01, 0xFF, 0xFE, 0x00, 0x00, 0x58, 0x52,
+                                            0x43, 0x45, 0x01, 0x00, 0x01, 0x0F , 0x0, 0x00,
+                                            0x77, 0x6B, 0x48, 0x5C, 0x43, 0x14, 0x1C, 0x34,
+                                            0x00};
+            *len = message.size();
+            *buf = input_buffer.data();
+            std::copy_n(message.begin(), *len, input_buffer.begin());
+            return true;
+        }
+        else if(std::string("CreateNoOk") == ::testing::UnitTest::GetInstance()->current_test_info()->name())
+        {
+            (void) buf; (void) len;
+            return false;
+        }
+        if(std::string("DeleteOk") == ::testing::UnitTest::GetInstance()->current_test_info()->name())
+        {
+            std::vector<uint8_t> message = {0x81, 0x00, 0x00, 0x00, 0x05, 0x01, 0x06, 0x00,
+                                            0x00, 0x02, 0xFF, 0xFE, 0x00, 0x00};
+            *len = message.size();
+            *buf = input_buffer.data();
+            std::copy_n(message.begin(), *len, input_buffer.begin());
+            return true;
+        }
+        else if(std::string("DeleteNoOk") == ::testing::UnitTest::GetInstance()->current_test_info()->name())
+        {
+            (void) buf; (void) len;
+            return false;
+        }
+
+        return false;
+    }
+
+    static uint8_t comm_error(void)
+    {
+        return 0;
+    }
+
+    static void on_status_func (struct uxrSession* session, uxrObjectId object_id, uint16_t request_id,
+                             uint8_t status, void* args)
+    {
+        (void) session; (void) object_id; (void) request_id; (void) status; (void) args;
+    }
+
+    static void on_topic_func (struct uxrSession* session, uxrObjectId object_id, uint16_t request_id,
+                             uxrStreamId stream_id, struct ucdrBuffer* ub, void* args)
+    {
+        (void) session; (void) object_id; (void) request_id; (void) stream_id; (void) ub; (void) args;
+    }
 };
 
-TEST_F(SessionTest, Reset)
-{
+SessionTest* SessionTest::current = nullptr;
 
+TEST_F(SessionTest, SetStatusCallback)
+{
+    int user_data;
+    uxr_set_status_callback(&session, on_status_func, &user_data);
+    EXPECT_EQ(session.on_status, on_status_func);
+    EXPECT_EQ(session.on_status_args, &user_data);
+}
+
+TEST_F(SessionTest, SetTopicCallback)
+{
+    int user_data;
+    uxr_set_topic_callback(&session, on_topic_func, &user_data);
+    EXPECT_EQ(session.on_topic, on_topic_func);
+    EXPECT_EQ(session.on_topic_args, &user_data);
+}
+
+TEST_F(SessionTest, CreateOk)
+{
+    bool created = uxr_create_session(&session);
+    ASSERT_TRUE(created);
+}
+
+TEST_F(SessionTest, CreateNoOk)
+{
+    bool created = uxr_create_session(&session);
+    ASSERT_FALSE(created);
+}
+
+TEST_F(SessionTest, DeleteOk)
+{
+    bool deleted = uxr_delete_session(&session);
+    ASSERT_TRUE(deleted);
+}
+
+TEST_F(SessionTest, DeleteNoOk)
+{
+    bool deleted = uxr_delete_session(&session);
+    ASSERT_FALSE(deleted);
+}
+
+TEST_F(SessionTest, RunTime)
+{
+    //TODO
+}
+
+TEST_F(SessionTest, RunUntilTimeout)
+{
+    //TODO
+}
+
+TEST_F(SessionTest, RunConfirmDelivery)
+{
+    //TODO
+}
+
+TEST_F(SessionTest, RunConfirmAllStatus)
+{
+    //TODO
+}
+
+TEST_F(SessionTest, RunConfirmOneStatus)
+{
+    //TODO
+}
+
+TEST_F(SessionTest, Flash)
+{
+    //TODO
 }
 
 // ****************************************************************************Y
 //                                  MOCKS
 // ****************************************************************************Y
 
-void uxr_init_stream_storage(uxrStreamStorage* storage)
-{
-    (void) storage;
-}
-
-void uxr_reset_stream_storage(uxrStreamStorage* storage)
-{
-    (void) storage;
-}
-
-uxrStreamId uxr_add_input_best_effort_buffer(uxrStreamStorage* storage)
-{
-    (void) storage;
-    return uxr_stream_id(0, UXR_BEST_EFFORT_STREAM, UXR_INPUT_STREAM);
-}
-
-uxrStreamId uxr_add_output_best_effort_buffer(uxrStreamStorage* storage, uint8_t* buffer, size_t size, uint8_t header_offset)
-{
-    (void) storage; (void) buffer; (void) size; (void) header_offset;
-    return uxr_stream_id(0, UXR_BEST_EFFORT_STREAM, UXR_OUTPUT_STREAM);
-}
-
-uxrStreamId uxr_add_input_reliable_buffer(uxrStreamStorage* storage, uint8_t* buffer, size_t size, uint16_t history, OnGetFragmentationInfo on_get_fragmentation_info)
-{
-    (void) storage; (void) buffer; (void) size; (void) history; (void) on_get_fragmentation_info;
-    return uxr_stream_id(0, UXR_RELIABLE_STREAM, UXR_INPUT_STREAM);
-}
-
-uxrStreamId uxr_add_output_reliable_buffer(uxrStreamStorage* storage, uint8_t* buffer, size_t size, uint16_t history, uint8_t header_offset, OnNewFragment on_new_fragment)
-{
-    (void) storage; (void) buffer; (void) size; (void) history; (void) header_offset; (void) on_new_fragment;
-    return uxr_stream_id(0, UXR_RELIABLE_STREAM, UXR_OUTPUT_STREAM);
-}
-
-uxrOutputBestEffortStream* uxr_get_output_best_effort_stream(uxrStreamStorage* storage, uint8_t index)
-{
-    (void) storage; (void) index;
-    return NULL;
-}
-
-uxrOutputReliableStream* uxr_get_output_reliable_stream(uxrStreamStorage* storage, uint8_t index)
-{
-    (void) storage; (void) index;
-    return NULL;
-}
-
-uxrInputBestEffortStream* uxr_get_input_best_effort_stream(uxrStreamStorage* storage, uint8_t index)
-{
-    (void) storage; (void) index;
-    return NULL;
-}
-
-uxrInputReliableStream* uxr_get_input_reliable_stream(uxrStreamStorage* storage, uint8_t index)
-{
-    (void) storage; (void) index;
-    return NULL;
-}
-
-bool uxr_output_streams_confirmed(const uxrStreamStorage* storage)
-{
-    (void) storage;
-    return false;
-}
-
-void uxr_init_session_info(uxrSessionInfo* info, uint8_t id, uint32_t key)
-{
-    (void) info; (void) id; (void) key;
-}
-
-void uxr_buffer_create_session(uxrSessionInfo* info, ucdrBuffer* ub, int64_t nanoseconds, uint16_t mtu)
-{
-    (void) info; (void) ub; (void) nanoseconds; (void)mtu;
-}
-
-void uxr_buffer_delete_session(uxrSessionInfo* info, ucdrBuffer* ub)
-{
-    (void) info; (void) ub;
-}
-
-void uxr_read_create_session_status(uxrSessionInfo* info, ucdrBuffer* ub)
-{
-    (void) info; (void) ub;
-}
-
-void uxr_read_delete_session_status(uxrSessionInfo* info, ucdrBuffer* ub)
-{
-    (void) info; (void) ub;
-}
-
-void uxr_stamp_create_session_header(const uxrSessionInfo* info, uint8_t* buffer)
-{
-    (void) info; (void) buffer;
-}
-
-void uxr_stamp_session_header(const uxrSessionInfo* info, uint8_t stream_id_raw, uxrSeqNum seq_num, uint8_t* buffer)
-{
-    (void) info; (void) stream_id_raw; (void) seq_num; (void) buffer;
-}
-
-bool uxr_read_session_header(const uxrSessionInfo* info, ucdrBuffer* ub, uint8_t* stream_id_raw, uxrSeqNum* seq_num)
-{
-    (void) info; (void) ub; (void) stream_id_raw; (void) seq_num;
-    return false;
-}
-
-uint8_t uxr_session_header_offset(const uxrSessionInfo* info)
-{
-    (void) info;
-    return 0;
-}
-
-void uxr_parse_base_object_request(const BaseObjectRequest* base, uxrObjectId* object_id, uint16_t* request_id)
-{
-    (void) base; (void) object_id; (void) request_id;
-}
-
 int64_t uxr_millis(void)
 {
-    return 0;
-}
-
-bool uxr_prepare_best_effort_buffer_to_write(uxrOutputBestEffortStream* stream, size_t size, struct ucdrBuffer* ub)
-{
-    (void) stream; (void) size; (void) ub;
-    return false;
-}
-
-bool uxr_prepare_best_effort_buffer_to_send(uxrOutputBestEffortStream* stream, uint8_t** buffer, size_t* length, uint16_t* seq_num)
-{
-    (void) stream; (void) buffer; (void) length; (void) seq_num;
-    return false;
-}
-
-bool uxr_prepare_reliable_buffer_to_write(uxrOutputReliableStream* stream, size_t size, size_t fragment_offset, struct ucdrBuffer* ub)
-{
-    (void) stream; (void) size; (void) fragment_offset; (void) ub;
-    return true;
-}
-
-bool uxr_prepare_next_reliable_buffer_to_send(uxrOutputReliableStream* stream, uint8_t** buffer, size_t* length, uxrSeqNum* seq_num)
-{
-    (void) stream; (void) buffer; (void) length; (void) seq_num;
-    return false;
-}
-
-bool uxr_update_output_stream_heartbeat_timestamp(uxrOutputReliableStream* stream, int64_t current_timestamp)
-{
-    (void) stream; (void) current_timestamp;
-    return false;
-}
-
-bool uxr_buffer_submessage_header(ucdrBuffer* ub, uint8_t submessage_id, uint16_t length, uint8_t flags)
-{
-    (void) ub; (void) submessage_id; (void) length; (void) flags;
-    return false;
-}
-
-bool uxr_read_submessage_header(ucdrBuffer* ub, uint8_t* submessage_id, uint16_t* length, uint8_t* flags)
-{
-    (void) ub; (void) submessage_id; (void) length; (void) flags;
-    return false;
-}
-
-size_t uxr_submessage_padding(size_t length)
-{
-    (void) length;
     return 0;
 }
