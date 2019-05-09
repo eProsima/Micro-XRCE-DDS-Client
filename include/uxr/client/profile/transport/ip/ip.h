cf3f2e4ece257bcfc4b74b2e4f78c7758576dc2f
julianbermudez <julianbermudez@eprosima.com>
2019-05-09 16:18:47 +0200

    Refs #5371. Add ip.h file.

---
+++ include/uxr/client/profile/transport/ip/ip.h
@@ -0,0 +1,34 @@
+// Copyright 2019 Proyectos y Sistemas de Mantenimiento SL (eProsima).
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef UXR_CLIENT_PROFILE_TRANSPORT_IP_IP_H_
+#define UXR_CLIENT_PROFILE_TRANSPORT_IP_IP_H_
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+typedef enum IpProtocol
+{
+    UXR_IPv4,
+    UXR_IPv6
+
+} IpProtocol;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // UXR_CLIENT_PROFILE_TRANSPORT_IP_IP_H_
