f77fedd7edafaff5921f2f170e9d38f902f50ee6
julibert <julianbermudez@eprosima.com>
2020-03-25 01:47:01 +0100

    Modify read_format_data function.

--- src/c/core/session/read_access.c
+++ src/c/core/session/read_access.c
@@ -154,7 +154,7 @@ inline void read_format_data(
         uint16_t request_id)
 {
     (void) length;
-    ub->last_data_size = 8; //reset alignment (as if we were created a new ucdrBuffer)
+//    ub->last_data_size = 8; //reset alignment (as if we were created a new ucdrBuffer)
 
     switch (object_id.type)
     {
@@ -167,7 +167,13 @@ inline void read_format_data(
 //                ucdr_init_buffer(&temp_buffer, ub->iterator, length);
 //                session->on_topic(session, object_id, request_id, stream_id, &temp_buffer, session->on_topic_args);
 //                ub->iterator += length;
-                session->on_topic(session, object_id, request_id, stream_id, ub, session->on_topic_args);
+
+//                session->on_topic(session, object_id, request_id, stream_id, ub, session->on_topic_args);
+
+                ucdrBuffer temp_buffer;
+                ucdr_copy_buffer(&temp_buffer, ub);
+                session->on_topic(session, object_id, request_id, stream_id, &temp_buffer, session->on_topic_args);
+                ucdr_advance_buffer(ub, length);
             }
             break;
         }
