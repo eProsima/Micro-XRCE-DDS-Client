3286ebe78030ae5c160acb3abc3455e5ac6ee150
Pablo Garrido <pablogs9@gmail.com>
2021-02-02 11:52:00 +0100

    Added continuous fragment mode (#177)
    * Initial
    
    * Working state
    
    * Second approach working state
    
    * Cleaning
    
    * Fix warnings
    
    * Updates
    
    * Fix warnings
    
    * Fix
    
    * Fix warnings
    
    * Check errors
    
    * Updates
    
    * Fix
    
    * Rename function
    
    * Add tests
    
    * Update CMakeLists.txt
    
    Co-authored-by: Jose Antonio Moral <joseantoniomoralparras@gmail.com>
    
    * Update examples/ContinousFragment/CMakeLists.txt
    
    Co-authored-by: Jose Antonio Moral <joseantoniomoralparras@gmail.com>
    
    * Update examples/ContinousFragment/main.c
    
    Co-authored-by: Jose Antonio Moral <joseantoniomoralparras@gmail.com>
    
    * Update include/uxr/client/core/session/write_access.h
    
    Co-authored-by: Jose Antonio Moral <joseantoniomoralparras@gmail.com>
    
    * Update src/c/core/session/write_access.c
    
    Co-authored-by: Jose Antonio Moral <joseantoniomoralparras@gmail.com>
    
    * Update src/c/core/session/write_access.c
    
    Co-authored-by: Jose Antonio Moral <joseantoniomoralparras@gmail.com>
    
    * Update src/c/core/session/write_access.c
    
    Co-authored-by: Jose Antonio Moral <joseantoniomoralparras@gmail.com>
    
    * Update src/c/core/session/write_access.c
    
    Co-authored-by: Jose Antonio Moral <joseantoniomoralparras@gmail.com>
    
    * Update include/uxr/client/core/session/write_access.h
    
    Co-authored-by: Jose Antonio Moral <joseantoniomoralparras@gmail.com>
    
    * Update examples/ContinousFragment/main.c
    
    Co-authored-by: Jose Antonio Moral <joseantoniomoralparras@gmail.com>
    
    * Update src/c/core/session/write_access.c
    
    Co-authored-by: Jose Antonio Moral <joseantoniomoralparras@gmail.com>
    
    * Update src/c/core/session/write_access.c
    
    Co-authored-by: Jose Antonio Moral <joseantoniomoralparras@gmail.com>
    
    * Update src/c/core/session/write_access.c
    
    Co-authored-by: Jose Antonio Moral <joseantoniomoralparras@gmail.com>
    
    * Fix indentation
    
    * Rename folder
    
    * Fix typo
    
    Co-authored-by: Jose Antonio Moral <joseantoniomoralparras@gmail.com>

--- src/c/core/session/write_access.c
+++ src/c/core/session/write_access.c
@@ -4,6 +4,10 @@
 #include "session_internal.h"
 #include "session_info_internal.h"
 #include "submessage_internal.h"
+#include "./stream/output_reliable_stream_internal.h"
+#include "./stream/common_reliable_stream_internal.h"
+#include "./stream/stream_storage_internal.h"
+#include "./stream/seq_num_internal.h"
 
 #define WRITE_DATA_PAYLOAD_SIZE 4
 #define SAMPLE_IDENTITY_SIZE    24
@@ -79,3 +83,139 @@ bool uxr_prepare_output_stream(uxrSession* session, uxrStreamId stream_id, uxrOb
     return !ub->error;
 }
 
+
+// Continuous fragment mode
+
+typedef struct {
+    uxrSession* session;
+    uxrOnBuffersFull flush_callback;
+    uxrStreamId stream_id;
+    size_t topic_size;
+} continuous_args_t;
+
+static continuous_args_t continuous_args;
+
+bool on_full_output_buffer_fragmented(ucdrBuffer* ub, void* args)
+{   
+    continuous_args_t * local_args = (continuous_args_t *) args;
+
+    uxrSession* session = local_args->session;
+    uxrOutputReliableStream* stream = uxr_get_output_reliable_stream(&session->streams, local_args->stream_id.index);
+
+    size_t remaining_blocks = get_available_free_slots(stream);
+
+    if (0 == remaining_blocks)
+    {
+        if(!local_args->flush_callback(session) ||
+           0 == (remaining_blocks = get_available_free_slots(stream)))
+        {
+            return true;
+        }
+    }
+
+    size_t buffer_capacity = uxr_get_reliable_buffer_capacity(&stream->base);
+    uint16_t available_block_size = (uint16_t)(buffer_capacity - (uint16_t)(stream->offset + SUBHEADER_SIZE));
+    local_args->topic_size = local_args->topic_size - (size_t) available_block_size;
+
+    ucdrBuffer temp_ub;
+    ucdr_init_buffer_origin_offset(
+            &temp_ub,
+            uxr_get_reliable_buffer(&stream->base, stream->last_written),
+            buffer_capacity,
+            0u,
+            uxr_get_reliable_buffer_size(&stream->base, stream->last_written));
+    uxr_buffer_submessage_header(&temp_ub, SUBMESSAGE_ID_FRAGMENT, available_block_size, (local_args->topic_size < buffer_capacity) ? FLAG_LAST_FRAGMENT : 0);
+    uxr_set_reliable_buffer_size(&stream->base, stream->last_written, buffer_capacity);
+    stream->last_written = uxr_seq_num_add(stream->last_written, 1);
+    
+    // Preparing the buffer for the user
+    ucdr_init_buffer(
+        ub,
+        temp_ub.iterator,
+        (size_t)(temp_ub.final - temp_ub.iterator));
+    ucdr_set_on_full_buffer_callback(ub, on_full_output_buffer_fragmented, args);
+
+    return false;
+}
+
+bool uxr_prepare_output_stream_fragmented(
+    uxrSession* session, 
+    uxrStreamId stream_id, 
+    uxrObjectId datawriter_id,
+    ucdrBuffer* ub, 
+    size_t topic_size,
+    uxrOnBuffersFull flush_callback)
+{
+    size_t user_required_space = topic_size + SUBHEADER_SIZE + WRITE_DATA_PAYLOAD_SIZE;
+
+    uxrOutputReliableStream* stream = uxr_get_output_reliable_stream(&session->streams, stream_id.index);
+
+    if (stream == NULL)
+    {
+        return false;
+    }
+
+    size_t remaining_blocks = get_available_free_slots(stream);
+    
+    if (0 == remaining_blocks)
+    {
+        if(!flush_callback(session) ||
+           0 == (remaining_blocks = get_available_free_slots(stream)))
+        {
+            return false;
+        }
+    }
+    
+    uxrSeqNum seq_num = stream->last_written;
+    uint8_t* buffer = uxr_get_reliable_buffer(&stream->base, seq_num);
+    size_t buffer_size = uxr_get_reliable_buffer_size(&stream->base, seq_num);
+    size_t buffer_capacity = uxr_get_reliable_buffer_capacity(&stream->base);
+
+    // Always start in a fresh buffer to avoid handling first fragment size
+    if(buffer_size > (size_t)stream->offset)
+    {
+        seq_num = uxr_seq_num_add(seq_num, 1);
+        buffer = uxr_get_reliable_buffer(&stream->base, seq_num);
+        buffer_size = uxr_get_reliable_buffer_size(&stream->base, seq_num);
+    }
+
+    uint16_t available_block_size = (uint16_t)(buffer_capacity - (uint16_t)(stream->offset + SUBHEADER_SIZE));
+
+    ucdrBuffer temp_ub;
+    ucdr_init_buffer_origin_offset(
+            &temp_ub,
+            uxr_get_reliable_buffer(&stream->base, seq_num),
+            buffer_capacity,
+            0u,
+            uxr_get_reliable_buffer_size(&stream->base, seq_num));
+    uxr_buffer_submessage_header(&temp_ub, SUBMESSAGE_ID_FRAGMENT, available_block_size, (user_required_space < buffer_capacity) ? FLAG_LAST_FRAGMENT : 0);
+    uxr_set_reliable_buffer_size(&stream->base, seq_num, buffer_capacity);
+    seq_num = uxr_seq_num_add(seq_num, 1);
+
+    // Preparing the buffer for the user
+    ucdr_init_buffer(
+        ub,
+        buffer + buffer_size + SUBHEADER_SIZE,
+        (uint32_t)(buffer_capacity - buffer_size - SUBHEADER_SIZE));
+
+    stream->last_written = seq_num;
+
+    // Fill the SUBMESSAGE_ID_WRITE_DATA
+    size_t payload_size = WRITE_DATA_PAYLOAD_SIZE + topic_size;
+
+    (void) uxr_buffer_submessage_header(ub, SUBMESSAGE_ID_WRITE_DATA, (uint16_t)payload_size, FORMAT_DATA);
+
+    WRITE_DATA_Payload_Data payload;
+    uxr_init_base_object_request(&session->info, datawriter_id, &payload.base);
+    (void) uxr_serialize_WRITE_DATA_Payload_Data(ub, &payload);
+
+    ucdr_init_buffer(ub, ub->iterator, (size_t)(ub->final - ub->iterator));
+
+    continuous_args.session = session;
+    continuous_args.stream_id = stream_id;
+    continuous_args.topic_size = user_required_space;    
+    continuous_args.flush_callback = flush_callback;    
+    ucdr_set_on_full_buffer_callback(ub, on_full_output_buffer_fragmented, &continuous_args);
+
+    return true;
+}
