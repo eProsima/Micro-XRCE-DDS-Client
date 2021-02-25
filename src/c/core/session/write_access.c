544dccbfdc28a72500bc61f3c87a6750c518007e
Pablo Garrido <pablogs9@gmail.com>
2021-02-25 12:21:13 +0100

    Add uxr_buffer_publish function

--- src/c/core/session/write_access.c
+++ src/c/core/session/write_access.c
@@ -63,6 +63,29 @@ uint16_t uxr_buffer_reply(
     return rv;
 }
 
+uint16_t uxr_buffer_publish(
+    uxrSession* session, 
+    uxrStreamId stream_id, 
+    uxrObjectId datawriter_id,
+    uint8_t* buffer,
+    size_t len)
+{
+    uint16_t rv = UXR_INVALID_REQUEST_ID;
+    ucdrBuffer ub;
+    size_t payload_size = WRITE_DATA_PAYLOAD_SIZE + len;
+
+    ub.error = !uxr_prepare_stream_to_write_submessage(session, stream_id, payload_size, &ub, SUBMESSAGE_ID_WRITE_DATA, FORMAT_DATA);
+    if (!ub.error)
+    {
+        WRITE_DATA_Payload_Data payload;
+        rv = uxr_init_base_object_request(&session->info, datawriter_id, &payload.base);
+        uxr_serialize_WRITE_DATA_Payload_Data(&ub, &payload);
+        ucdr_serialize_array_uint8_t(&ub, buffer, len);
+    }
+
+    return rv;
+}
+
 bool uxr_prepare_output_stream(uxrSession* session, uxrStreamId stream_id, uxrObjectId datawriter_id,
                                ucdrBuffer* ub, uint32_t topic_size)
 {
@@ -82,7 +105,6 @@ bool uxr_prepare_output_stream(uxrSession* session, uxrStreamId stream_id, uxrOb
 
     return !ub->error;
 }
-
 
 // Continuous fragment mode
 
