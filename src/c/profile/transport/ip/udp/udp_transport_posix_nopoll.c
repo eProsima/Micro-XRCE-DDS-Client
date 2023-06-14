8bad43a0f07f427b0ba74c6d72573c5245b792af
Beat Küng <beat-kueng@gmx.net>
2023-06-14 14:17:33 +0200

    fix udp_transport: call freeaddrinfo only if getaddrinfo succeeded
    Otherwise freeaddrinfo can access invalid memory.
    
    Signed-off-by: Beat Küng <beat-kueng@gmx.net>

--- src/c/profile/transport/ip/udp/udp_transport_posix_nopoll.c
+++ src/c/profile/transport/ip/udp/udp_transport_posix_nopoll.c
@@ -55,8 +55,8 @@ bool uxr_init_udp_platform(
                     break;
                 }
             }
+            freeaddrinfo(result);
         }
-        freeaddrinfo(result);
     }
     return rv;
 }
