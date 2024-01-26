cdd4120757956e4dc159b833e1c8cbc74c9eaf3c
Pablo Garrido <pablogs9@gmail.com>
2024-01-26 08:02:46 +0100

    Add BigHelloWorld example (#379)
    * Add BigHelloWorld example
    
    Signed-off-by: Pablo Garrido <pablogs9@gmail.com>
    
    * Fix printf
    
    Signed-off-by: Pablo Garrido <pablogs9@gmail.com>
    
    * Fix windows
    
    * Fix warning
    
    Signed-off-by: Pablo Garrido <pablogs9@gmail.com>
    
    ---------
    
    Signed-off-by: Pablo Garrido <pablogs9@gmail.com>

--- examples/PublishBigHelloWorld/main.c
+++ examples/PublishBigHelloWorld/main.c
@@ -12,41 +12,25 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "HelloWorld.h"
+#include "BigHelloWorld.h"
 
 #include <uxr/client/client.h>
+#include <ucdr/microcdr.h>
 
 #include <stdio.h> //printf
 #include <string.h> //strcmp
 #include <stdlib.h> //atoi
+#include <assert.h> //assert
 
-#define STREAM_HISTORY  8
+#define STREAM_HISTORY  128
 #define BUFFER_SIZE     UXR_CONFIG_UDP_TRANSPORT_MTU* STREAM_HISTORY
-
-void on_topic(
-        uxrSession* session,
-        uxrObjectId object_id,
-        uint16_t request_id,
-        uxrStreamId stream_id,
-        struct ucdrBuffer* ub,
-        uint16_t length,
-        void* args)
-{
-    (void) session; (void) object_id; (void) request_id; (void) stream_id; (void) length;
-
-    HelloWorld topic;
-    HelloWorld_deserialize_topic(ub, &topic);
-
-    printf("Received topic: %s, id: %d\n", topic.message, topic.index);
-
-    uint32_t* count_ptr = (uint32_t*) args;
-    (*count_ptr)++;
-}
 
 int main(
         int args,
         char** argv)
 {
+    assert(BUFFER_SIZE > BIG_HELLO_WORLD_STRING_SIZE);
+
     // CLI
     if (3 > args || 0 == atoi(argv[2]))
     {
@@ -55,11 +39,8 @@ int main(
     }
 
     char* ip = argv[1];
-    char* port =  argv[2];
+    char* port = argv[2];
     uint32_t max_topics = (args == 4) ? (uint32_t)atoi(argv[3]) : UINT32_MAX;
-
-    // State
-    uint32_t count = 0;
 
     // Transport
     uxrUDPTransport transport;
@@ -71,8 +52,7 @@ int main(
 
     // Session
     uxrSession session;
-    uxr_init_session(&session, &transport.comm, 0xCCCCDDDD);
-    uxr_set_topic_callback(&session, on_topic, &count);
+    uxr_init_session(&session, &transport.comm, 0xAAAABBBB);
     if (!uxr_create_session(&session))
     {
         printf("Error at create session.\n");
@@ -85,8 +65,7 @@ int main(
                     STREAM_HISTORY);
 
     uint8_t input_reliable_stream_buffer[BUFFER_SIZE];
-    uxrStreamId reliable_in = uxr_create_input_reliable_stream(&session, input_reliable_stream_buffer, BUFFER_SIZE,
-                    STREAM_HISTORY);
+    uxr_create_input_reliable_stream(&session, input_reliable_stream_buffer, BUFFER_SIZE, STREAM_HISTORY);
 
     // Create entities
     uxrObjectId participant_id = uxr_object_id(0x01, UXR_PARTICIPANT_ID);
@@ -103,54 +82,59 @@ int main(
     uxrObjectId topic_id = uxr_object_id(0x01, UXR_TOPIC_ID);
     const char* topic_xml = "<dds>"
             "<topic>"
-            "<name>HelloWorldTopic</name>"
-            "<dataType>HelloWorld</dataType>"
+            "<name>BigHelloWorldTopic</name>"
+            "<dataType>BigHelloWorld</dataType>"
             "</topic>"
             "</dds>";
     uint16_t topic_req = uxr_buffer_create_topic_xml(&session, reliable_out, topic_id, participant_id, topic_xml,
                     UXR_REPLACE);
 
-    uxrObjectId subscriber_id = uxr_object_id(0x01, UXR_SUBSCRIBER_ID);
-    const char* subscriber_xml = "";
-    uint16_t subscriber_req = uxr_buffer_create_subscriber_xml(&session, reliable_out, subscriber_id, participant_id,
-                    subscriber_xml, UXR_REPLACE);
+    uxrObjectId publisher_id = uxr_object_id(0x01, UXR_PUBLISHER_ID);
+    const char* publisher_xml = "";
+    uint16_t publisher_req = uxr_buffer_create_publisher_xml(&session, reliable_out, publisher_id, participant_id,
+                    publisher_xml, UXR_REPLACE);
 
-    uxrObjectId datareader_id = uxr_object_id(0x01, UXR_DATAREADER_ID);
-    const char* datareader_xml = "<dds>"
-            "<data_reader>"
+    uxrObjectId datawriter_id = uxr_object_id(0x01, UXR_DATAWRITER_ID);
+    const char* datawriter_xml = "<dds>"
+            "<data_writer>"
             "<topic>"
             "<kind>NO_KEY</kind>"
-            "<name>HelloWorldTopic</name>"
-            "<dataType>HelloWorld</dataType>"
+            "<name>BigHelloWorldTopic</name>"
+            "<dataType>BigHelloWorld</dataType>"
             "</topic>"
-            "</data_reader>"
+            "</data_writer>"
             "</dds>";
-    uint16_t datareader_req = uxr_buffer_create_datareader_xml(&session, reliable_out, datareader_id, subscriber_id,
-                    datareader_xml, UXR_REPLACE);
+    uint16_t datawriter_req = uxr_buffer_create_datawriter_xml(&session, reliable_out, datawriter_id, publisher_id,
+                    datawriter_xml, UXR_REPLACE);
 
     // Send create entities message and wait its status
     uint8_t status[4];
     uint16_t requests[4] = {
-        participant_req, topic_req, subscriber_req, datareader_req
+        participant_req, topic_req, publisher_req, datawriter_req
     };
     if (!uxr_run_session_until_all_status(&session, 1000, requests, status, 4))
     {
-        printf("Error at create entities: participant: %i topic: %i subscriber: %i datareader: %i\n", status[0],
+        printf("Error at create entities: participant: %i topic: %i publisher: %i datawriter: %i\n", status[0],
                 status[1], status[2], status[3]);
         return 1;
     }
 
-    // Request topics
-    uxrDeliveryControl delivery_control = {
-        0
-    };
-    delivery_control.max_samples = UXR_MAX_SAMPLES_UNLIMITED;
-    uxr_buffer_request_data(&session, reliable_out, datareader_id, reliable_in, &delivery_control);
-
-    // Read topics
+    // Write topics
     bool connected = true;
+    uint32_t count = 0;
     while (connected && count < max_topics)
     {
+        BigHelloWorld topic;
+        topic.index = count++;
+        memset(topic.message, 'A', sizeof(topic.message) - 1);
+        topic.message[sizeof(topic.message) - 1] = '\0';
+
+        ucdrBuffer ub;
+        uint32_t topic_size = BigHelloWorld_size_of_topic(&topic, 0);
+        uxr_prepare_output_stream(&session, reliable_out, datawriter_id, &ub, topic_size);
+        BigHelloWorld_serialize_topic(&ub, &topic);
+
+        printf("Send topic with size %d, id: %d\n", (uint32_t)strlen(topic.message), topic.index);
         connected = uxr_run_session_time(&session, 1000);
     }
 
